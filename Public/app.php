c6925a139a43af4ef50f22b3d03078d6d952cf16
tomkys144 <tomkys144@gmail.com>
2020-12-26 17:06:00 +0100

    basic routing added
    Signed-off-by: tomkys144 <tomkys144@gmail.com>

--- Public/app.php
+++ Public/app.php
@@ -1,2 +1,54 @@
 <?php
+
+use App\Controllers\ItemsController;
+use App\Controllers\SessionController;
+use App\Controllers\UserController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
 require_once __DIR__ . '/../bootstrap.php';
+
+$app = AppFactory::create();
+
+$app->post('/items/{method}', function (Request $request, Response $response) {
+    $method = $request->getAttribute('method');
+    $sessionID = $request->getAttribute('sessionID');
+    $SessionController = new SessionController();
+    $ItemsController = new ItemsController();
+
+    if ($sessionID === false) {
+        $SessionController->startSession();
+    } else {
+        $SessionController->startSession($sessionID);
+    }
+
+    $result = call_user_func_array(array($ItemsController, $method), $request->getAttribute('Args'));
+
+    $data = json_encode($result['DATA']);
+
+    $response->getBody()->write($data);
+
+    return $response->withStatus($result['CODE']);
+});
+
+$app->post('/user/{method}', function (Request $request, Response $response) {
+    $method = $request->getAttribute('method');
+    $sessionID = $request->getAttribute('sessionID');
+    $SessionController = new SessionController();
+    $UserController = new UserController();
+
+    if ($sessionID === false) {
+        $SessionController->startSession();
+    } else {
+        $SessionController->startSession($sessionID);
+    }
+
+    $result = call_user_func_array(array($UserController, $method), json_decode($request->getAttribute('Args')));
+
+    $data = json_encode($result['DATA']);
+
+    $response->getBody()->write($data);
+
+    return $response->withStatus($result['CODE']);
+});
--- bootstrap.php
+++ bootstrap.php
@@ -1,15 +1,8 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Services\SessionService;
 use Dotenv\Dotenv;
 
-$session = new SessionService;
-if (isset($_REQUEST['session_id'])) {
-    $session->startSession($_REQUEST['session_id']);
-} else {
-    $session->startSession();
-}
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->required(['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASSWORD'])->notEmpty();
